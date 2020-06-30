d7412a092a86679ebe8ff2748d8f9577640a42d1
esteban1225 <exteban.1225@gmail.com>
2020-06-30 18:27:10 -0500

    Initial maven configuration

---
+++ src/test/java/com/washcarapp/people/PeopleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.washcarapp.people;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeopleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
